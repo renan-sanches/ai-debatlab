244ceecb8d4a315355849e85b9a1a770e0cfda60
Renan Sanches <rsanches@bluecore.com>
2025-12-29 23:52:17 -0500

    fix: lazy-load vite in dev only to remove prod dependency

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -3,10 +3,14 @@ import fs from "fs";
 import { type Server } from "http";
 import { nanoid } from "nanoid";
 import path from "path";
-import { createServer as createViteServer } from "vite";
-import viteConfig from "../../vite.config";
 
 export async function setupVite(app: Express, server: Server) {
+  // Load Vite lazily so production bundle doesn't require the dev dependency
+  const [{ createServer: createViteServer }, { default: viteConfig }] = await Promise.all([
+    import("vite"),
+    import("../../vite.config"),
+  ]);
+
   const serverOptions = {
     middlewareMode: true,
     hmr: { server },
